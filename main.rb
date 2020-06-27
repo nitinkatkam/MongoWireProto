963f5764b6c633436fde7c41f33aa17cf0f6bc4c
nitinkatkam <nitinkatkam@gmail.com>
2020-06-27 08:30:02 +0400

    Fix: has_key? call looks for port string instead of port label

--- main.rb
+++ main.rb
@@ -14,10 +14,10 @@ require 'optparse'
 def start_in_mode(cmdline_params)
     case cmdline_params[:mode]
     when 'server'
-        s = cmdline_params.has_key?('port') ? Server.new(cmdline_params[:port]) : Server.new
+        s = cmdline_params.has_key?(:port) ? Server.new(cmdline_params[:port]) : Server.new
         s.start
     when 'client'
-        c = cmdline_params.has_key?('port') ? Client.new(cmdline_params[:port]) : Client.new
+        c = cmdline_params.has_key?(:port) ? Client.new(cmdline_params[:port]) : Client.new
         c.start
     end
 
